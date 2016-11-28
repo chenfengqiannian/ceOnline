9cf8f57d5d0f8c9a5fc7f29cef9f2e245addc07e
mac <k564398853@163.com>
2016-11-28 16:21:23 +0800

    didian

--- ceoOnline/models.py
+++ ceoOnline/models.py
@@ -7,8 +7,9 @@ from django.db import models
 class user(models.Model):
     phone=models.CharField(u'手机号',max_length=255)
     password=models.CharField(u'密码',max_length=255)
-    jianjie = models.CharField(u'简介', max_length=255,blank=True)
+    jianjie = models.TextField(u'简介')
     zhiwu = models.CharField(u'职务', max_length=255,blank=True)
+    dizhi=models.CharField(u'地址',max_length=255,blank=True)
     touxiang = models.ImageField(u'头像',blank=True)
     image1 = models.ImageField(u'图像1',blank=True)
     image2 = models.ImageField(u'图像2',blank=True)
@@ -16,7 +17,9 @@ class user(models.Model):
     image4 = models.ImageField(u'图像4',blank=True)
     image5 = models.ImageField(u'图像5',blank=True)
     isVIP=models.BooleanField(u'是否金色名称')
+    bankuai=models.CharField(u'板块',max_length=255,blank=True)
     onlinetime=models.DateTimeField()
+    didian=models.ForeignKey('tb_prov_city_area_street',blank=True,null=True)
 class setting(models.Model):
     name=models.CharField(u'名称',max_length=255)
     value=models.TextField(u'内容')
@@ -31,3 +34,8 @@ class friend(models.Model):
     statechoice=((0,u'请求'),(1,u'好友'))
     state=models.IntegerField(choices=statechoice,default=0)
     createtime=models.DateTimeField(auto_now=True)
+class tb_prov_city_area_street(models.Model):
+    code=models.IntegerField(blank=True)
+    parentId=models.IntegerField(blank=True)
+    name=models.CharField(max_length=50,blank=True)
+    level=models.IntegerField(blank=True)
--- ceoOnline/views.py
+++ ceoOnline/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
-
+from django.http import *
+from .models import *
 # Create your views here.
+import json
+def userinapi(request):
+    try:
+        if request.method == 'POST':
+
+            data = json.loads(request.body)
+
+            dataphone = data['phone']
+            leixing = data.get('leixing', '-1')
+            if (leixing == '0'):
+                if (user.objects.get(phone=data['phone']).userpw == data['userpw']):
+                    return HttpResponse("ok")
+                else:
+                    return HttpResponseForbidden()
+            if (leixing == '1'):
+                if (len(user.objects.filter(phone=data['phone'])) >= 1):
+                    return HttpResponseForbidden()
+                else:
+
+                    myuser = user.objects.create(phone=data['phone'], userpw=data['userpw'])
+
+                    # myuser.didianchar.add(mydidian)
+                    return HttpResponse('ok')
+            if (leixing == '2'):
+                userpwxiugai = user.objects.get(phone=data['phone'])
+                if (userpwxiugai.userpw == data['olduserpw']):
+                    userpwxiugai.userpw = data['newuserpw']
+                    userpwxiugai.save()
+                    return HttpResponse('ok')
+                else:
+                    return HttpResponseForbidden()
+
+
+            clphone = user.objects.get(phone=dataphone)
+            reqkey = data.keys()
+
+            for k in reqkey:
+                setattr(clphone, k, data[k])
+            clphone.save()
+            return HttpResponse("ok")
+    except Exception, e:
+            print e
+            return HttpResponseServerError(e)
